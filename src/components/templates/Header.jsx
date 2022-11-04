3fd633cd2355457ecfffff9b3272156372c00216
b_oxygen <rhdtkqkr@gmail.com>
2022-11-04 15:47:39 +0900

    loginWithKaikas 구현

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -3,8 +3,10 @@ import SearchIcon from "@components/atoms/SearchIcon";
 import Logo from "@components/atoms/Logo";
 import HamburgerIcon from "@components/atoms/HamburgerIcon";
 import * as colors from "@styles/colors";
+import kaikasImgUrl from "@assets/image/kaikas.png";
+import { toast } from "react-toastify";
+import useAuth from "@hooks/useAuth";
 import Wallet from "@components/atoms/Wallet";
-import KaiKas_image from "@assets/image/kaikas.png";
 
 const Container = styled.header`
   width: 100%;
@@ -42,6 +44,7 @@ const GrayRoundBox = styled.div`
   height: 40px;
   border-radius: 20px;
   background-color: ${colors.bgSecondary};
+  cursor: pointer;
 `;
 
 const WalletBox = styled(GrayRoundBox)`
@@ -53,7 +56,47 @@ const SearchIconWrapper = styled.div`
   margin-left: 16px;
 `;
 
+const KaikasImage = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const klaytn = window.klaytn;
+
 function Header() {
+  const { user, setUser } = useAuth();
+  async function loginWithKaikas() {
+    if (!klaytn) {
+      toast.error("kaikas 설치되었는 지 확인해주세요.", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
+    }
+
+    try {
+      //[0x2asdfsggg]
+      const accounts = await toast.promise(
+        klaytn.enable(),
+        { pending: "Kaikas 지갑 연동 중" },
+        { closeButton: true }
+      );
+      setUser(accounts[0]);
+      toast.success(`${accounts[0].slice(0, 13)}...님 환영합니다 ^^`);
+    } catch {
+      toast.error("로그인에 실패하셨습니다. 다시 시도해주세요 !");
+    }
+
+    // 로그인 로직
+  }
+
+  function handleLogin() {
+    loginWithKaikas();
+  }
+
+  function handleDone() {
+    toast.success("이미 로그인이 되셨습니다.");
+  }
+
   return (
     <Container>
       <LogoWrapper>
@@ -64,8 +107,8 @@ function Header() {
           <SearchIcon />
         </SearchIconWrapper>
       </SearchBarWrapper>
-      <WalletBox>
-        <Wallet />
+      <WalletBox onClick={user ? handleDone : handleLogin}>
+        {user ? <KaikasImage src={kaikasImgUrl} /> : <Wallet />}
       </WalletBox>
       <GrayRoundBox>
         <HamburgerIcon />
